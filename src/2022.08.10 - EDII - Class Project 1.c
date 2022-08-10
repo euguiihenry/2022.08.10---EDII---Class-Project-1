6dfddabe6763c23a426207f1689b3e94bfa8f3ae
Guilherme <1A226881906@fumeccad.local>
2022-08-10 08:58:09 -0300

    update - torre de hanói

--- src/2022.08.10 - EDII - Class Project 1.c
+++ src/2022.08.10 - EDII - Class Project 1.c
@@ -11,7 +11,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
-}
+// Prototypes:
+	void moveTorre(int, char, char, char); // Just for the language to know that this function exists.
+
+// Functions:
+	int main(void) {
+		moveTorre(3, 'A', 'C', 'B');
+		return EXIT_SUCCESS;
+	}
+
+	void moveTorre(int n, char orig, char dest, char aux) {
+		// Caso Base:
+			if (n == 1) {
+				printf("\nMover disco 1 da torre %c para a torre %c", orig, dest);
+				return;
+			}
+
+		// Caso Recursivo:
+			moveTorre((n-1), orig, aux, dest);
+			printf("\nMover disco %i da torre %c para a torre %c"), n, orig, dest;
+			moveTorre((n-1), aux, dest, orig);
+	}
